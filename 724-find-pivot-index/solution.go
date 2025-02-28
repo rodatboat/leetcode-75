1878bcb3bfdd7387c959eeafea26dc2501f3b5a2
rodatboat <ronaldo3055@gmail.com>
2025-02-28 11:19:26 -0800

    adding 724 in golang

--- 724-find-pivot-index/solution.go
+++ 724-find-pivot-index/solution.go
@@ -1,5 +1,26 @@
 package main
 
 func pivotIndex(nums []int) int {
-    
+    n := len(nums)
+    ridx := n - 1
+    l_sum := make([]int, n)
+    r_sum := make([]int, n)
+
+    for idx, i := range nums {
+        if idx == 0 {
+            l_sum[idx] = i
+            r_sum[ridx] = nums[ridx]
+        } else {
+            l_sum[idx] = i + l_sum[idx-1]
+            r_sum[ridx] = r_sum[ridx+1] + nums[ridx]
+        }
+        ridx--
+    }
+
+    for i := 0; i < n; i++ {
+        if l_sum[i] == r_sum[i] {
+            return i
+        }
+    }
+    return -1
 }
