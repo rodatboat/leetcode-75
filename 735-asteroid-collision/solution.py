adf7ce8e14df865c9641f7afdd4af40e899afea1
rodatboat <ronaldo3055@gmail.com>
2025-03-02 16:24:28 -0800

    merged condition for 735, for cleaner solution

--- 735-asteroid-collision/solution.py
+++ 735-asteroid-collision/solution.py
@@ -8,19 +8,17 @@ class Solution:
         for idx, i in enumerate(asteroids):
             while True:
                 top = self.getTop(a)
-                if top == None or top*i > 0:
+                if i > 0 or top == None or top*i > 0:
                     a.append(i)
                     break
-
-                if i > 0:
-                    a.append(i)
+                if abs(top) > abs(i):
+                    # if top is gt, it wins, so dont add i to stack
+                    break
+                elif abs(top) == abs(i):
+                    a.pop()
+                    # if top == i, its a draw. a is (--) from the stack, and i never gets (++) to stack top
                     break
                 else:
-                    if abs(top) > abs(i):
-                        break
-                    elif abs(top) == abs(i):
-                        a.pop()
-                        break
-                    else:
-                        a.pop()
+                    # if 
+                    a.pop()
         return a
