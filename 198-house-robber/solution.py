31ad0e0323186005a6ca5ade0081c240122de9c8
rodatboat <ronaldo3055@gmail.com>
2025-05-29 18:02:07 -0500

    added second solution to 198

--- 198-house-robber/solution.py
+++ 198-house-robber/solution.py
@@ -22,3 +22,15 @@ class Solution:
     
     # DP Bottom-Up Approach (aka Tabulation)
     def rob2(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        dp = [0] * n
+        for i in range(n):
+            if i == 0:
+                dp[i] = nums[0]
+            elif i == 1:
+                dp[i] = max(nums[0], nums[1])
+            else:
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
